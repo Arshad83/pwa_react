dd1c5324c5fa49e1b4f10956f45c720ae43f6397
Arshad83 <arshad.syed483@gmail.com>
2018-10-27 14:49:12 +0530

    hot reloader for development

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App=()=>{
+	return <h1>Hello from React! abcd jklm </h1>
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
-var React=require('react');
-var ReactDOM=require('react-dom');
-ReactDOM.render(React.createElement('h1', false, 'Hello from react!'), 
-      document.getElementById('root'));
+/*var React=require('react');
+var ReactDOM=require('react-dom');*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'
+// ReactDOM.render(React.createElement('h1', false, 'Hello from react!'), 
+//       document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+
+if(module.hot){
+	module.hot.accept('./App',()=>{
+		const NextApp=require('./App').default;
+		ReactDOM.render(
+			<App />,
+			document.getElementById('root')
+			);
+	});
+}
